bb3709a268864b2f59eb300e53c1ceb1213c36c1
45-YamazakiMiyuki <kero0.0v@gmail.com>
2015-08-30 16:51:05 +0900

    初回コミット

---
+++ 5/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<form action="get_confirm.php" method="get">
+	名前: <input type="text" name="name" />
+	年齢: <input type="text" name="age" />
+	<input type="submit" />
+</form>
+<form action="post_confirm.php" method="post">
+	名前: <input type="text" name="name" />
+	年齢: <input type="text" name="age" />
+	<input type="submit" />
+</form>
+<ul>
+<li><a href="hensu.php">変数</a></li>
+<li><a href="hairetsu.php">配列</a></li>
+<li><a href="seigyo.php">制御構文</a></li>
+<li><a href="kansu.php">関数</a></li>
+</ul>
+
+<ul>
+<li><a href="write.php">ファイル書き込み</a></li>
+<li><a href="read.php">ファイル読み込む</a></li>
+</ul>
+
+<ul>
+<li><a href="input_enq.php">アンケート入力ページ</a></li>
+<li><a href="show_enp.php">アンケート表示ページ</a></li>
+</ul>
+
+</body>
+</html>
